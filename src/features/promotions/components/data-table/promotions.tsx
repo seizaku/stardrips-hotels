8f755fe1d5ffd5a9a44d07a9b96794d41120d0ed
seizaku.dev <seizaku.dev@gmail.com>
2024-12-01 00:17:33 +0800

    fix type

--- src/features/promotions/components/data-table/promotions.tsx
+++ src/features/promotions/components/data-table/promotions.tsx
@@ -3,17 +3,12 @@
 import { api } from "~/trpc/react";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
-import { type Promotion } from "~/server/api/routers/promos/router";
 
 const Promotions = () => {
   const { data, isLoading } = api.promotion.fetchAll.useQuery();
 
   return (
-    <DataTable
-      columns={columns}
-      data={(data ?? []) as Promotion[]}
-      isLoading={isLoading}
-    />
+    <DataTable columns={columns} data={data ?? []} isLoading={isLoading} />
   );
 };
 
