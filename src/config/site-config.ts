8e4bedc54cb3248b5b9d288d3e10ab6f66a0d9bf
seizaku.dev <seizaku.dev@gmail.com>
2024-12-01 03:51:03 +0800

    feat: add site config

---
+++ src/config/site-config.ts
@@ -0,0 +1,26 @@
+const siteConfig = {
+  navigationLinks: [
+    {
+      title: "Hotel / Hotel Groups",
+      href: "/hotels",
+    },
+    {
+      title: "Emails",
+      href: "/emails",
+    },
+    {
+      title: "Promotions",
+      href: "/promotions",
+    },
+    {
+      title: "Matched Listings",
+      href: "/matched",
+    },
+    {
+      title: "Unmatched Listings",
+      href: "/unmatched",
+    },
+  ],
+};
+
+export { siteConfig };
