8fe0a39ac0f7ed248545d73b48ec026689d1e9f1
seizaku.dev <seizaku.dev@gmail.com>
2024-12-08 03:14:35 +0800

    feat: oauth router

---
+++ src/server/api/routers/auth/router.ts
@@ -0,0 +1,32 @@
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { GoogleAuth } from "~/server/api/common/oauth";
+import { z } from "zod";
+
+const authRouter = createTRPCRouter({
+  getAuthURL: publicProcedure.query(async () => {
+    const client = new GoogleAuth();
+    return client.auth.generateAuthUrl({
+      access_type: "offline",
+      scope: [
+        "https://www.googleapis.com/auth/gmail.modify",
+        "https://www.googleapis.com/auth/gmail.readonly",
+      ],
+      prompt: "consent",
+    });
+  }),
+  callback: publicProcedure
+    .input(
+      z.object({
+        code: z.string(),
+      }),
+    )
+    .query(async ({ input }) => {
+      const client = new GoogleAuth();
+      const result = await client.auth.getToken(input.code.toString());
+      client.auth.setCredentials(result.tokens);
+
+      return result.tokens;
+    }),
+});
+
+export { authRouter };
