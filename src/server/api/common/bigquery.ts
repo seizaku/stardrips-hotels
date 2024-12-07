b84170a251cc6afba61c8159367af271c67ab0c6
seizaku.dev <seizaku.dev@gmail.com>
2024-12-08 03:16:38 +0800

    refactor: remove constructor option and add return type to query method

--- src/server/api/common/bigquery.ts
+++ src/server/api/common/bigquery.ts
@@ -1,33 +1,27 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import {
-  BigQuery as BigQueryClient,
-  type BigQueryOptions,
-} from "@google-cloud/bigquery";
+import { BigQuery as BigQueryClient, type Query } from "@google-cloud/bigquery";
 import { env } from "~/env";
 
 export class BigQuery {
   public client: BigQueryClient;
 
-  constructor(options?: BigQueryOptions) {
+  constructor() {
     this.client = new BigQueryClient({
       projectId: process.env.PROJECT_ID,
       credentials: {
         client_email: env.SERVICE_EMAIL,
         private_key: (env.SERVICE_PRIVATE_KEY ?? "").replace(/\\n/g, "\n"),
       },
-      ...options,
     });
   }
 
-  async query<T>(params: { query: string; params?: any }): Promise<T[]> {
+  async query<T>(params: Query): Promise<T[]> {
     try {
       const [rows] = await this.client.query({
         query: params.query,
         params: params.params,
       });
-      return rows;
+
+      return rows as T[];
     } catch (error) {
       console.error("Error querying BigQuery:", error);
       return [];
