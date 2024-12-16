0f81b7f320cba086ed2a4e37f1ed538f5bb0bcf4
Null <seizaku.dev@gmail.com>
2024-12-16 22:43:12 +0800

    feat: reusable server side pagination and search

---
+++ src/components/pagination-datatable.tsx
@@ -0,0 +1,71 @@
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+
+interface PaginationDatatable {
+  maxSize?: number;
+}
+
+const PaginationDataTable = ({ maxSize }: PaginationDatatable) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [page, setPage] = useState(1);
+  const [value] = useDebounce(page, 100);
+
+  const limit = parseInt(searchParams.get("limit") ?? "100");
+  const maxPage = Math.ceil((maxSize && maxSize / limit) ?? 0);
+
+  useEffect(() => {
+    const currentParams = Object.fromEntries(searchParams.entries());
+
+    const updatedParams = new URLSearchParams({
+      ...currentParams,
+      page: value.toString(),
+    });
+
+    router.push(`${window.location.origin}/${pathname}?${updatedParams.toString()}`);
+  }, [value])
+
+  return <>
+    <div className="flex items-center justify-between space-x-2 py-4">
+      <div className="flex w-full max-w-lg items-center gap-2 text-sm text-muted-foreground">
+        Page
+        <Input
+          min={1}
+          max={maxPage}
+          type="number"
+          className="w-16"
+          defaultValue={searchParams.get("page") ?? 1}
+          onChange={(e) => {
+            const value = parseInt(e.currentTarget.value);
+            if (value > maxPage) return;
+            setPage(value);
+          }}
+        />
+        of {maxPage}
+      </div>
+      <div className="flex gap-2">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => setPage(prev => prev - 1)}
+          disabled={page == 1}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => setPage(prev => prev + 1)}
+          disabled={page == maxPage}
+        >
+          Next
+        </Button>
+      </div>
+    </div></>;
+};
+
+export { PaginationDataTable };
---
+++ src/components/search-datatable.tsx
@@ -0,0 +1,65 @@
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+import { useDebounce } from "use-debounce";
+import { Input } from "./ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger } from "./ui/select";
+import { Rows2Icon } from "lucide-react";
+
+const SearchDataTable = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [text, setText] = useState("");
+  const [limit, setLimit] = useState(100);
+  const [textValue] = useDebounce(text, 200);
+  const [limitValue] = useDebounce(limit, 200);
+
+  useEffect(() => {
+    const currentParams = Object.fromEntries(searchParams.entries());
+
+    const updatedParams = new URLSearchParams({
+      ...currentParams,
+      query: textValue,
+      limit: limitValue.toString(),
+    });
+
+    router.push(`${window.location.origin}/${pathname}?${updatedParams.toString()}`);
+  }, [textValue, limitValue]);
+
+  return (
+    <>
+      <Input
+        type="text"
+        className="w-96"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Search values"
+      />
+      <Select
+        onValueChange={(value: string) => setLimit(parseInt(value))}
+        value={limitValue.toString()}
+      >
+        <SelectTrigger className="hidden w-fit md:flex">
+          <Rows2Icon className="h-4 w-4" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Max Rows</SelectLabel>
+            <SelectItem value="5">5</SelectItem>
+            <SelectItem value="10">10</SelectItem>
+            <SelectItem value="20">20</SelectItem>
+            <SelectItem value="40">40</SelectItem>
+            <SelectItem value="60">60</SelectItem>
+            <SelectItem value="80">80</SelectItem>
+            <SelectItem value="100">100</SelectItem>
+            <SelectItem value="200">200</SelectItem>
+            <SelectItem value="300">300</SelectItem>
+            <SelectItem value="400">400</SelectItem>
+            <SelectItem value="500">500</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select ></>
+  );
+};
+
+export { SearchDataTable };
