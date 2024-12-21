a2055ef14adf997a74dcd8a5fbefd356c9b4b035
Null <seizaku.dev@gmail.com>
2024-12-22 02:50:32 +0800

    config: allowed hostnames

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cf.bstatic.com",
+      }
+    ]
+  }
+};
 
 export default config;
